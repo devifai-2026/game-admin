6c286632f7deecd22ad73864a1d6ac78e4cfbc77
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2026-02-26 19:27:40 +0530

    add prod url

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Create base instance with default config
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000/api/v1',
+  baseURL: 'https://devifai.website/api/v1',
   timeout: 30000, // 30 seconds timeout
   headers: {
     'Content-Type': 'application/json',
